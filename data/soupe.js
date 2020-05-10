6f5294904eaaf83be58769c161aee82847402e7d
Valouleloup <gohan_3_7@hotmail.com>
2020-05-10 18:57:31 +0200

    Add data files

---
+++ data/soupe.js
@@ -0,0 +1,127 @@
+var soupes = [
+    {
+        "name": "Soupe Liquide Velouté de 9 Légumes - Knorr",
+        "id": "8714100766576",
+        "nutriscore": "b",
+        "nutriments": {
+            "fiber_100g": 0.9,
+            "saturated-fat_100g": 0.2,
+            "fat_100g": 1.9,
+            "sodium_100g": 0.26,
+            "nutrition-score-fr_100g": 2,
+            "proteins_100g": 0.6,
+            "carbohydrates_100g": 6.3,
+            "sugars_100g": 2.1,
+            "salt_100g": 0.65,
+            "energy-kcal_100g": 201,
+            "energy-kcal_value": 48,
+            "energy_100g": 200,
+            "energy-kj_100g": 200,
+            "nova-group_100g": 4,
+            "fruits-estimate_100g": 30.8
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 30.8,
+            "energy": 200,
+            "positive_points": 0,
+            "negative_points": 2,
+            "score": 2,
+            "sodium_points": 2,
+            "fiber_points": 0,
+            "sugars_points": 0,
+            "proteins_points": 0,
+            "energy_points": 0,
+            "saturated_fat_points": 0,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 0
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:vegetarian",
+            "en:european-vegetarian-union",
+            "en:green-dot"
+        ]
+    },
+    {
+        "name": "Velouté de Légumes du Soleil - Liebig",
+        "id": "3036811365585",
+        "nutriscore": "b",
+        "nutriments": {
+            "fiber_100g": 1.1,
+            "saturated-fat_100g": 0.3,
+            "fat_100g": 1.7,
+            "sodium_100g": 0.28,
+            "nutrition-score-fr_100g": 0,
+            "proteins_100g": 1.1,
+            "carbohydrates_100g": 4.3,
+            "sugars_100g": 2.6,
+            "salt_100g": 0.7,
+            "energy-kcal_100g": 163,
+            "energy-kcal_value": 39,
+            "energy_100g": 163,
+            "nova-group_100g": 4,
+            "fruits-estimate_100g": 77.6
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 77.6,
+            "energy": 163,
+            "positive_points": 3,
+            "negative_points": 3,
+            "score": 0,
+            "sodium_points": 3,
+            "fiber_points": 1,
+            "sugars_points": 0,
+            "proteins_points": 0,
+            "energy_points": 0,
+            "saturated_fat_points": 0,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 2
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:fsc",
+            "en:fsc-mix",
+            "en:green-dot",
+            "en:made-in-france"
+        ]
+    },
+    {
+        "name": "Velouté potiron à la crème fraîche - Knorr",
+        "id": "8710908962042",
+        "nutriscore": "c",
+        "nutriments": {
+            "fiber_100g": 0.9,
+            "saturated-fat_100g": 1.2,
+            "fat_100g": 1.9,
+            "sodium_100g": 0.292,
+            "nutrition-score-fr_100g": 3,
+            "proteins_100g": 0.7,
+            "carbohydrates_100g": 3.9,
+            "sugars_100g": 2.2,
+            "salt_100g": 0.73,
+            "energy-kcal_100g": 159,
+            "energy-kcal_value": 38,
+            "energy_100g": 158,
+            "energy-kj_100g": 158,
+            "nova-group_100g": 4,
+            "fruits-estimate_100g": 49.6
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 49.6,
+            "energy": 158,
+            "positive_points": 1,
+            "negative_points": 4,
+            "score": 3,
+            "sodium_points": 3,
+            "fiber_points": 0,
+            "sugars_points": 0,
+            "proteins_points": 0,
+            "energy_points": 0,
+            "saturated_fat_points": 1,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 1
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:sustainable-farming",
+            "en:green-dot"
+        ]
+    }
+];
---
+++ data/yaourt.js
@@ -0,0 +1,275 @@
+var yaourts = [
+    {
+        "name": "Framboise delicate - Les 2 vaches",
+        "nutriscore": "c",
+        "type": "yaourt",
+        "nutriments": {
+            "saturated-fat_100g": 2.1,
+            "fat_100g": 3,
+            "sodium_100g": 0.052,
+            "nutrition-score-fr_100g": 3,
+            "proteins_100g": 3.7,
+            "carbohydrates_100g": 11.9,
+            "sugars_100g": 11.9,
+            "salt_100g": 0.13,
+            "energy-kcal_100g": 377,
+            "energy-kcal_value": 90,
+            "energy_100g": 377,
+            "nova-group_100g": 4
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 7,
+            "energy": 377,
+            "positive_points": 2,
+            "negative_points": 5,
+            "score": 3,
+            "sodium_points": 0,
+            "fiber_points": 0,
+            "sugars_points": 2,
+            "proteins_points": 2,
+            "energy_points": 1,
+            "saturated_fat_points": 2,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 0
+        },
+        "origins": "france",
+        "labels_tags": [
+            "en:organic",
+            "en:eu-organic",
+            "en:eu-non-eu-agriculture",
+            "en:fr-bio-01",
+            "en:green-dot",
+            "fr:ab-agriculture-biologique",
+            "en:lait-francais-french-milk"
+        ]
+    },
+    {
+        "name": "Panier de Yoplait - J'aime les fruits rouge",
+        "nutriscore": "b",
+        "type": "yaourt",
+        "nutriments": {
+            "saturated-fat_100g": 1.6,
+            "fat_100g": 2.5,
+            "sodium_100g": 0.048,
+            "nutrition-score-fr_100g": 2,
+            "proteins_100g": 3.3,
+            "carbohydrates_100g": 13.3,
+            "sugars_100g": 12.8,
+            "salt_100g": 0.12,
+            "energy-kcal_100g": 389,
+            "energy-kcal_value": 93,
+            "energy_100g": 389,
+            "nova-group_100g": 4
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 18.2,
+            "energy": 389,
+            "positive_points": 2,
+            "negative_points": 4,
+            "score": 2,
+            "sodium_points": 0,
+            "fiber_points": 0,
+            "sugars_points": 2,
+            "proteins_points": 2,
+            "energy_points": 1,
+            "saturated_fat_points": 1,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 0
+        },
+        "origins": "",
+        "labels_tags": []
+    },
+    {
+        "name": "Velouté Fruix - Danone",
+        "id": "3033490004651",
+        "nutriscore": "b",
+        "type": "yaourt",
+        "nutriments": {
+            "saturated-fat_100g": 2,
+            "fat_100g": 2.9,
+            "sodium_100g": 0.04,
+            "nutrition-score-fr_100g": 2,
+            "proteins_100g": 3.4,
+            "carbohydrates_100g": 13.4,
+            "sugars_100g": 13,
+            "salt_100g": 0.1,
+            "energy-kcal_100g": 393,
+            "energy-kcal_value": 94,
+            "energy_100g": 393,
+            "nova-group_100g": 4
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 5,
+            "energy": 393,
+            "positive_points": 2,
+            "negative_points": 4,
+            "score": 2,
+            "sodium_points": 0,
+            "fiber_points": 0,
+            "sugars_points": 2,
+            "proteins_points": 2,
+            "energy_points": 1,
+            "saturated_fat_points": 1,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 0
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:no-preservatives"
+        ]
+    },
+    {
+        "name": "Compote pomme poire - Andros",
+        "id": "3608580006657",
+        "nutriscore": "a",
+        "type": "yaourt",
+        "nutriments": {
+            "fiber_100g": 2.1,
+            "saturated-fat_100g": 0,
+            "fat_100g": 0.2,
+            "sodium_100g": 0,
+            "nutrition-score-fr_100g": -4,
+            "proteins_100g": 0.3,
+            "carbohydrates_100g": 17,
+            "sugars_100g": 17,
+            "salt_100g": 0,
+            "energy-kcal_100g": 314,
+            "energy-kcal_value": 75,
+            "energy_100g": 314,
+            "nova-group_100g": 3
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 85,
+            "energy": 314,
+            "positive_points": 7,
+            "negative_points": 3,
+            "score": -4,
+            "sodium_points": 0,
+            "fiber_points": 2,
+            "sugars_points": 3,
+            "proteins_points": 0,
+            "energy_points": 0,
+            "saturated_fat_points": 0,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 5
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:green-dot",
+            "en:no-colorings",
+            "en:no-preservatives",
+            "fr:source-de-vitamine-c"
+        ]
+    },
+    {
+        "name": "Compote pomme fraise - Andros",
+        "id": "3608580006671",
+        "nutriscore": "a",
+        "type": "yaourt",
+        "nutriments": {
+            "fiber_100g": 1.3,
+            "saturated-fat_100g": 0,
+            "fat_100g": 0.2,
+            "sodium_100g": 0,
+            "nutrition-score-fr_100g": -3,
+            "proteins_100g": 0.3,
+            "carbohydrates_100g": 17,
+            "sugars_100g": 17,
+            "salt_100g": 0,
+            "energy-kcal_100g": 310,
+            "energy-kcal_value": 74,
+            "energy_100g": 310,
+            "nova-group_100g": 3
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 85,
+            "energy": 310,
+            "positive_points": 6,
+            "negative_points": 3,
+            "score": -3,
+            "sodium_points": 0,
+            "fiber_points": 1,
+            "sugars_points": 3,
+            "proteins_points": 0,
+            "energy_points": 0,
+            "saturated_fat_points": 0,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 5
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:no-colorings",
+            "en:no-preservatives"
+        ]
+    },
+    {
+        "name": "Activia vanille",
+        "nutriscore": "b",
+        "type": "yaourt",
+        "nutriments": {
+            "saturated-fat_100g": 2,
+            "fat_100g": 2.9,
+            "sodium_100g": 0.052,
+            "nutrition-score-fr_100g": 2,
+            "proteins_100g": 3.7,
+            "carbohydrates_100g": 12.7,
+            "sugars_100g": 12.7,
+            "salt_100g": 0.13,
+            "energy_100g": 386,
+            "energy-kj_100g": 386,
+            "nova-group_100g": 4
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 0,
+            "energy": 386,
+            "positive_points": 2,
+            "negative_points": 4,
+            "score": 2,
+            "sodium_points": 0,
+            "fiber_points": 0,
+            "sugars_points": 2,
+            "proteins_points": 2,
+            "energy_points": 1,
+            "saturated_fat_points": 1,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 0
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:nutriscore",
+            "en:nutriscore-grade-b"
+        ]
+    },
+    {
+        "name": "Activia fraise",
+        "id": "3033491325250",
+        "nutriscore": "b",
+        "nutriments": {
+            "saturated-fat_100g": 2,
+            "fat_100g": 2.8,
+            "sodium_100g": 0.052,
+            "nutrition-score-fr_100g": 2,
+            "proteins_100g": 3.7,
+            "carbohydrates_100g": 11.6,
+            "sugars_100g": 11.5,
+            "salt_100g": 0.13,
+            "energy_100g": 366,
+            "energy-kj_100g": 366,
+            "nova-group_100g": 4,
+            "fruits-estimate_100g": 8.4
+        },
+        "nutriscore_data": {
+            "fruits_vegetables_nuts_colza_walnut_olive_oils": 8.4,
+            "energy": 366,
+            "positive_points": 2,
+            "negative_points": 4,
+            "score": 2,
+            "sodium_points": 0,
+            "fiber_points": 0,
+            "sugars_points": 2,
+            "proteins_points": 2,
+            "energy_points": 1,
+            "saturated_fat_points": 1,
+            "fruits_vegetables_nuts_colza_walnut_olive_oils_points": 0
+        },
+        "origins": "",
+        "labels_tags": [
+            "en:nutriscore",
+            "en:nutriscore-grade-b"
+        ]
+    }
+];
